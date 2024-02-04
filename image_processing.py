5a3ec486d00be5e8b6d50649710f838f851116bc
mtrzm <mtrzm>
2024-02-04 21:06:23 +0100

    Add image comparison to tests.

--- image_processing.py
+++ image_processing.py
@@ -1,8 +1,59 @@
-"""Library for reading and processing images."""
 from pathlib import Path
-from typing import Tuple
+from typing import Optional, Tuple
 
+import cv2
 from PIL import Image
+from skimage.metrics import structural_similarity
+
+
+class COLORS:
+    BLACK = (0, 0, 0)
+
+
+def ssim(
+    image_1: Path, image_2: Path, threshold: float = 0.99
+) -> Tuple[float, Optional[Path]]:
+    """Compares images using Structural Similarity Index.
+
+    Code adapted from: https://stackoverflow.com/a/71634759
+
+    If SSIM is below provided threshold, the comparison image will be generated.
+
+    Args:
+        image_1: path to first file for comparison
+        image_2: path to second file for comparison
+        threshold: threshold for image equality
+
+    Returns:
+        SSIM score and optionally path to comparison image
+    """
+    img1 = cv2.imread(str(image_1), cv2.IMREAD_GRAYSCALE)
+    img2 = cv2.imread(str(image_2), cv2.IMREAD_GRAYSCALE)
+
+    score, diff = structural_similarity(img1, img2, full=True)
+    # print(f"Similarity Score: {score * 100:.2f}%")
+
+    if score >= threshold:
+        return score, None
+
+    diff = (diff * 255).astype("uint8")
+
+    thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
+
+    # reread the image to get colored comparison
+    img2 = cv2.imread(str(image_2))
+
+    for c in contours:
+        area = cv2.contourArea(c)
+        if area > 100:
+            x, y, w, h = cv2.boundingRect(c)
+            cv2.rectangle(img2, (x, y), (x + w, y + h), COLORS.BLACK, 2)
+    
+    diff = image_1.parent / f"diff-{image_1.name}-{image_2.name}.png"
+    cv2.imwrite(str(diff), img2)
+    return score, diff
 
 
 def get_size(im_path: Path) -> Tuple[int, int]:
--- test_images.py
+++ test_images.py
@@ -2,22 +2,33 @@ import pathlib
 
 import pytest
 
+import image_processing
 import lib_appium
 
+TEST_DATA = pathlib.Path("test_data")
+IMAGE_1 = TEST_DATA / "IMAGE_1.png"
+IMAGE_2 = TEST_DATA / "IMAGE_2.png"
+EXPORTED = TEST_DATA / "IMAGE_1.jpg"
+THRESHOLD = 0.99
 
-IMAGE_1 = pathlib.Path("test_data/IMAGE_1.png")
-IMAGE_2 = pathlib.Path("test_data/IMAGE_2.png")
 
-
-@pytest.mark.parametrize("path", [IMAGE_1])
-def test_screenshot(driver, path):
+@pytest.mark.parametrize("input_image", [IMAGE_1])
+def test_load_image(driver, input_image):
     screenshot_path = lib_appium.take_screenshot(driver)
+    resized = image_processing.resize_to_fit(screenshot_path, input_image)
+    score, comparison = image_processing.ssim(input_image, resized, THRESHOLD)
 
-    assert screenshot_path.exists()
+    assert score > THRESHOLD, f"Image similarity ({score:.2f}) below threshold ({THRESHOLD}), difference saved to: {comparison.absolute()}"
 
 
-@pytest.mark.parametrize("path", [IMAGE_1])
-def test_export(driver, path):
+@pytest.mark.parametrize("input_image", [IMAGE_1])
+def test_export(driver, input_image):
     exported = lib_appium.export_file(driver)
 
     assert exported.exists()
+
+
+@pytest.mark.parametrize("exported_image, reference", [(EXPORTED, IMAGE_2)])
+def test_compare_exported(exported_image, reference):
+    score, comparison = image_processing.ssim(exported_image, reference, THRESHOLD)
+    assert score > THRESHOLD, f"Image similarity ({score:.2f}) below threshold ({THRESHOLD}), difference saved to: {comparison.absolute()}"
